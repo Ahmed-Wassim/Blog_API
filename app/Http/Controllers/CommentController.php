90d35c5d9f6f67865110b662ebb0d86e4eb734b6
Ahmed-Wassim <ahmedwassim317@gmail.com>
2024-03-01 23:50:17 +0200

    implment comment in the post

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
 
 class CommentController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    public function store(StoreCommentRequest $post)
+    {
+        $post->comments()->create([
+            'user_id' => auth()->user()->id,
+            'body' => request('body'),
+        ]);
+
+        return $this->sendSuccess($post->comments, 201);
+    }
 }
